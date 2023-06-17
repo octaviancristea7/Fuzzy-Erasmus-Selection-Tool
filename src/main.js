3f90311dfd1e4ab5208d00251b3e0a02185dac3a
Octavian Cristea <octavian.cristea@student.unitbv.ro>
2023-06-17 16:39:03 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './assets/main.css'
+import PrimeVue from 'primevue/config'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(PrimeVue)
+
+app.mount('#app')
