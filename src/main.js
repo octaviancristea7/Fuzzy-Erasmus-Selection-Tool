116e313dbc4d533ad2073adff9dda525fa3f7f1d
Octavian Cristea <octavian.cristea@student.unitbv.ro>
2023-06-18 19:46:31 +0300

    Data table implemented (part 1)

--- src/main.js
+++ src/main.js
@@ -1,30 +1,61 @@
-import "./assets/main.css";
-import PrimeVue from "primevue/config";
-
 import { createApp } from "vue";
 import App from "./App.vue";
+import PrimeVue from "primevue/config";
 import router from "./router";
-
 //theme and icons
-//import 'primevue/resources/themes/soho-light/theme.css'
-import "primevue/resources/themes/mdc-light-indigo/theme.css";
+import "primevue/resources/themes/soho-light/theme.css";
+// import "primevue/resources/themes/mdc-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
 import "primeflex/primeflex.css";
 
 //components
-
-//directives
-
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+import MultiSelect from "primevue/multiselect";
+import ToggleButton from "primevue/togglebutton";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import ColumnGroup from "primevue/columngroup"; // optional
+import Row from "primevue/row";
+import Paginator from "primevue/paginator";
+import Tooltip from "primevue/tooltip";
+import Dialog from "primevue/dialog";
+import InputNumber from "primevue/inputnumber";
+import InputSwitch from "primevue/inputswitch";
+import Dropdown from "primevue/dropdown";
+import Checkbox from "primevue/checkbox";
 import ToastService from "primevue/toastservice";
+import TabView from 'primevue/tabview';
+import TabPanel from 'primevue/tabpanel';
+import Toolbar from 'primevue/toolbar';
+
+
 
 const app = createApp(App);
-
 app.use(router);
 app.use(PrimeVue);
 app.use(ToastService);
-
 app.mount("#app");
 
 //components
+app.component("Button", Button);
+app.component("Column", Column);
+app.component("ColumnGroup", ColumnGroup);
+app.component("DataTable", DataTable);
+app.component("Dialog", Dialog);
+app.component("InputText", InputText);
 app.component("ToastService", ToastService);
+app.component("Checkbox", Checkbox);
+app.component("InputSwitch", InputSwitch);
+app.component("Dropdown", Dropdown);
+app.component("InputNumber", InputNumber);
+app.component("Paginator", Paginator);
+app.component("Row", Row);
+app.component("MultiSelect", MultiSelect);
+app.component("ToggleButton", ToggleButton);
+app.component("TabView", TabView);
+app.component("TabPanel", TabPanel);
+app.component("Toolbar", Toolbar);
+//directives
+app.directive("tooltip", Tooltip);
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,172 @@
+<template>
+  <TabView>
+    <TabPanel header="Tabel note">
+      <Toolbar class="mb-4">
+        <template #start>
+          <Button
+            label="ADAUGĂ STUDENT"
+            icon="pi pi-plus"
+            severity="success"
+            class="mr-2"
+            @click="openNew"
+          />
+        </template>
+      </Toolbar>
+      <DataTable
+        :value="students"
+        tableStyle="min-width: 50rem"
+        paginator
+        :rows="5"
+        :rowsPerPageOptions="[5, 10, 20, 50]"
+      >
+        <Column
+          field="name"
+          header="Nume student"
+          bodyClass="text-center"
+          sortable
+        ></Column>
+        <Column
+          field="firstSemester"
+          header="Medie semestrul I"
+          bodyClass="text-center"
+          sortable
+        ></Column>
+        <Column
+          field="secondSemester"
+          header="Medie semestrul II"
+          bodyClass="text-center"
+          sortable
+        ></Column>
+        <Column
+          field="foreignLanguage"
+          header="Notă limbă străină"
+          bodyClass="text-center"
+          sortable
+        ></Column>
+        <Column
+          :exportable="false"
+          style="min-width: 8rem"
+          header="Acțiuni"
+          bodyClass="text-center"
+        >
+          <template #body="slotProps">
+            <Button
+              icon="pi pi-pencil"
+              outlined
+              rounded
+              class="mr-2"
+              @click="editStudent(slotProps.students)"
+            />
+            <Button
+              icon="pi pi-trash"
+              outlined
+              rounded
+              severity="danger"
+              @click="confirmDeleteStudent(slotProps.students)"
+            />
+          </template>
+        </Column>
+      </DataTable>
+    </TabPanel>
+    <TabPanel header="Tabel fuzzy">
+      <DataTable
+        :value="students"
+        tableStyle="min-width: 50rem"
+        paginator
+        :rows="5"
+        :rowsPerPageOptions="[5, 10, 20, 50]"
+      >
+        <Column
+          field="name"
+          header="Nume student"
+          bodyClass="text-center"
+          sortable
+        ></Column>
+        <Column
+          field="firstSemester"
+          header="Medie semestrul I"
+          bodyClass="text-center"
+          sortable
+        ></Column>
+        <Column
+          field="secondSemester"
+          header="Medie semestrul II"
+          bodyClass="text-center"
+          sortable
+        ></Column>
+        <Column
+          field="foreignLanguage"
+          header="Notă limbă străină"
+          bodyClass="text-center"
+          sortable
+        ></Column>
+      </DataTable>
+    </TabPanel>
+    <Dialog
+      v-model:visible="deleteStudentDialog"
+      :style="{ width: '450px' }"
+      header="Confirm"
+      :modal="true"
+    >
+      <div class="confirmation-content">
+        <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
+        <span v-if="student"
+          >Are you sure you want to delete <b>{{ student.name }}</b
+          >?</span
+        >
+      </div>
+      <template #footer>
+        <Button
+          label="No"
+          icon="pi pi-times"
+          text
+          @click="deleteStudentDialog = true"
+        />
+        <Button label="Yes" icon="pi pi-check" text @click="deleteStudent" />
+      </template>
+    </Dialog>
+  </TabView>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from "vue";
+
+
+const deleteStudentDialog = ref(false);
+const student = ref({});
+const students = ref([
+  {
+    name: "Student 1",
+    firstSemester: 8.9,
+    secondSemester: 9.4,
+    foreignLanguage: 8.7,
+  },
+  {
+    name: "Student 2",
+    firstSemester: 9.5,
+    secondSemester: 9.5,
+    foreignLanguage: 8,
+  },
+  {
+    name: "Student 3",
+    firstSemester: 10,
+    secondSemester: 7.5,
+    foreignLanguage: 9,
+  },
+]);
+
+const confirmDeleteStudent = () => {
+  deleteStudentDialog.value = true;
+  console.log("se intampla ceva");
+};
+
+const openNew = () => {
+  console.log("OpenNew function TBE");
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+.p-column-header-content {
+  justify-content: center;
+}
+</style>
